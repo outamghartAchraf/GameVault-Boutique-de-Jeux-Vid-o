bf45c7b175ef66b0c889dc41a7a8e2f8f164a51f
outamghartAchraf <outamghart2000@gmail.com>
2026-04-01 15:34:23 +0100

    add code for get Eelement Dom

--- app.js
+++ app.js
@@ -2,3 +2,22 @@ import { games } from './data.js';
 let cart = [];
 let activeGenre = 'Tous';
 let searchQuery = '';
+
+const catalogueEl = document.getElementById('catalogue');
+const emptyStateEl = document.getElementById('emptyState');
+const searchInput = document.getElementById('searchInput');
+const filtersEl = document.getElementById('filters');
+const countEl = document.getElementById('resultCount');
+
+const cartBtn = document.getElementById('cartBtn');
+const badgeEl = document.getElementById('cartBadge');
+const cartPage = document.getElementById('cartPage');
+const backBtn = document.getElementById('backBtn');
+
+const cartListEl = document.getElementById('cartList');
+const cartEmptyEl = document.getElementById('cartEmpty');
+const cartFooterEl = document.getElementById('cartFooter');
+const cartTotalEl = document.getElementById('cartTotal');
+const orderBtn = document.getElementById('orderBtn');
+
+const toastEl = document.getElementById('toast');

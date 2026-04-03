48c723131305ccd96ce7250059aa601c9a7f42d3
outamghartAchraf <outamghart2000@gmail.com>
2026-04-03 11:13:14 +0100

    add folder images

---
+++ js/data.js
@@ -0,0 +1,15 @@
+export const games = [
+  { id:1,  title:"Cyberstrike Zero",    genre:"Action", price:49.99, image: "./assets/images/1.jpg" },  
+  { id:2,  title:"Legends of Aethoria", genre:"RPG",    price:59.99, image: "./assets/images/2.jpg" },  
+  { id:3,  title:"Phantom Recon",       genre:"FPS",    price:44.99, image: "./assets/images/3.jpg" },   
+  { id:4,  title:"Steel Uprising",      genre:"Action", price:39.99, image: "./assets/images/4.jpg" },   
+  { id:5,  title:"Chrono Rift RPG",     genre:"RPG",    price:54.99, image: "./assets/images/5.jpg" },   
+  { id:6,  title:"Warzone Delta",       genre:"FPS",    price:34.99, image: "./assets/images/6.jpg" },   
+  { id:7,  title:"Neon Rampage",        genre:"Action", price:29.99, image: "./assets/images/7.jpg" },  
+  { id:8,  title:"Shadow Protocol",     genre:"FPS",    price:49.99, image: "./assets/images/8.jpg" },      
+  { id:9,  title:"Dragon's Oath",       genre:"RPG",    price:64.99, image: "./assets/images/9.jpg" },  
+  { id:10, title:"Hyper Clash X",       genre:"Action", price:24.99, image: "./assets/images/10.jpg" },  
+  { id:11, title:"Apex Infiltrator",    genre:"FPS",    price:39.99, image: "./assets/images/11.jpg" },  
+  { id:12, title:"Eternal Kingdoms",    genre:"RPG",    price:69.99, image: "./assets/images/12.jpg" },  
+
+]
